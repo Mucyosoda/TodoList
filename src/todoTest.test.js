cd5de5ea650ce9a226d210db03a62d800d0bd6d4
Mucyosoda <mucyosofa@gmail.com>
2021-10-14 20:17:10 +0100

    Update localstorage

--- src/todoTest.test.js
+++ src/todoTest.test.js
@@ -4,13 +4,9 @@ const { dom } = require('./testdom.js');
 
 describe(' add new task', () => {
   test('check addTaskList function', () => {
-    // Arrange
     let testArray = [1, 2, 3, 4];
-
-    // Act
     testArray = addTaskList(testArray, 'Add new task');
 
-    // Assert
     const latestElement = testArray[testArray.length - 1];
     expect(testArray).toHaveLength(5);
     expect(latestElement.completed).toBe(false);
@@ -25,15 +21,14 @@ describe(' add new task', () => {
 
 describe('remove task', () => {
   test('check removeTaskList function', () => {
-    // Arrange
     const itemtodo = { index: 3 };
     const id = '3';
-    // Act
     const result = removeTaskList(itemtodo, id);
-    // Assert
     expect(result).toBe(false);
   });
   test('should not have childs', () => {
-    expect(dom.window.document.querySelector('#test-2').childElementCount).toBe(1);
+    expect(dom.window.document.querySelector('#test-2').childElementCount).toBe(
+      1,
+    );
   });
 });
