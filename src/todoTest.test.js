520ccbf1d6d383b8463dc8f67d0de00eb0a04d0f
basnetlaxmi <ddsanzel@gmail.com>
2021-10-14 12:08:25 +0545

    test file for add and remove

--- src/todoTest.test.js
+++ src/todoTest.test.js
@@ -1,1 +1,30 @@
 import { addTaskList, removeTaskList } from './todoTest.js';
+
+describe(' add new task', () => {
+    test('check addTaskList function', () => {
+      // Arrange
+      let testArray = [1, 2, 3, 4];
+  
+      // Act
+      testArray = addTaskList(testArray, 'Add new task');
+  
+      // Assert
+      const latestElement = testArray[testArray.length - 1];
+      expect(testArray).toHaveLength(5);
+      expect(latestElement.completed).toBe(false);
+      expect(latestElement.description).toBe('Add new task');
+      expect(latestElement.index).toBe(testArray.length);
+    });
+  });
+  
+  describe('remove task', () => {
+    test('check removeTaskList function', () => {
+      // Arrange
+      const itemtodo = { index: 3 };
+      const id = '3';
+      // Act
+      const result = removeTaskList(itemtodo, id);
+      // Assert
+      expect(result).toBe(false);
+    });
+  });
